2eed9e520cb035e8917e21d5f90ac2a5880071ed
Nithesh Bhaskar <nitheshbhaskar@gmail.com>
2023-09-29 22:30:51 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,5 +1,8 @@
 public class app{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a=3, b=5, c=6;
+        int sum = a+b+c;
+        System.out.println("The sum is: "+sum);
     }
 }
